a3ab0cdd0464976e37d977ae0881213203b4d70a
Vasya Pankov <frostkslo1@yandex.ru>
2022-10-06 00:16:52 +0300

    4 Задания

--- DataBase/MainWindow.xaml.cs
+++ DataBase/MainWindow.xaml.cs
@@ -15,32 +15,54 @@ namespace DataBase
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string QueryString = "SELECT * FROM Students WHERE Familia LIKE ?";
-        private const string ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\user\Desktop\labs\04.01\db_lab1\DataBase\db\College.mdb;Persist Security Info=False";
+        private string QueryGetMalesWithAge =
+            "SELECT *, DateDiff(\"yyyy\", Datarogd, Date()) AS [Возраст]  FROM Students WHERE Students.Pol = 'М' AND Familia LIKE ?";
+        private string baseQuery = "SELECT * FROM Students WHERE Familia LIKE ?";
+        private string queryNow;
+        private const string ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\pankSU\Desktop\db_lab1\DataBase\db\College.mdb;Persist Security Info=False";
         OleDbConnection connection = new OleDbConnection(ConnectionString);
         public MainWindow()
         {
             InitializeComponent();
-            connection.Open();
+            reloadTablebyCommand(baseQuery);
+        }
+
+        public void reloadTablebyCommand(string? Query = null, string Familia = "%")
+        {
+            try
+            {
+                connection.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("База данных не найдена или что-то пошло не так.");
+                Close();
+                return;
+            }
+            
             var table = new DataTable();
-            var command = new OleDbCommand(QueryString, connection);
-            command.Parameters.Add( "?", OleDbType.VarChar, 80 ).Value = "%";
-            var adapter = new OleDbDataAdapter(command);
-            adapter.Fill(table);
+            var command = new OleDbCommand(Query ?? queryNow, connection);
+            command.Parameters.Add( "?", OleDbType.VarChar, 80 ).Value = Familia;
+            new OleDbDataAdapter(command).Fill(table);
             data.ItemsSource = table.DefaultView;
             connection.Close();
+            queryNow = Query ?? queryNow;
         }
 
         private void Search(object sender, RoutedEventArgs e)
         {
-            connection.Open();
-            var table = new DataTable();
-            var command = new OleDbCommand(QueryString, connection);
-            command.Parameters.Add( "?", OleDbType.VarChar, 80 ).Value = $"{Familia.Text}%";
-            var adapter = new OleDbDataAdapter(command);
-            adapter.Fill(table);
-            data.ItemsSource = table.DefaultView;
-            connection.Close();
+            reloadTablebyCommand(Familia: Familia.Text);
+        }
+
+        private void ToggleButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            reloadTablebyCommand(QueryGetMalesWithAge);
+        }
+
+
+        private void ToggleButton_OnUnchecked(object sender, RoutedEventArgs e)
+        {
+            reloadTablebyCommand(baseQuery);
         }
     }
 }
